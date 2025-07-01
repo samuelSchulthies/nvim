6a7e5a0cfb79bf42c702ee7d6ce9eba45ee560dd
Sam Schulthies <samalpine@xof.pixar.com>
2025-07-01 14:26:30 -0700

    Minor changes in debugging bashls

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -28,9 +28,9 @@ return {
 			lspconfig.clangd.setup({
 				capabilities = capabilities,
 			})
-			lspconfig.bashls.setup({
+			-- lspconfig.bashls.setup({
                 -- capabilities = capabilities,
-			})
+			-- })
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
 			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
 			vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, {})
