43ca1f8d3841e8c56907959524648fb920492a8d
Sam Schulthies <samalpine@xof.pixar.com>
2025-07-11 17:18:14 -0700

    added terminal keybinding

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -3,6 +3,5 @@ vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 vim.cmd("set number")
-vim.g.mapleader = " " 
-
-
+vim.g.mapleader = " "
+vim.keymap.set('n', '<leader>t', ':botright new | resize 10 | terminal<CR>', {})
